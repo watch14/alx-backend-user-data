2423fb5f3150e3794542512b113e08351b744486
watch14”git config --global user.name watch14” <watchman1414@gmail.com>
2024-05-30 03:37:45 +0100

    fast_push

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -6,12 +6,17 @@ import re
 from typing import List
 
 
-def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
+def filter_datum(
+        fields: List[str], redaction: str, message: str, separator: str
+        ) -> str:
     """
     filtered dat
     """
     pattern = f"({'|'.join(fields)})=.+?{separator}"
-    return re.sub(pattern, lambda m: f"{m.group(1)}={redaction}{separator}", message)
+    return re.sub(
+            pattern, lambda m: f"{m.group(1)}={redaction}{separator}", message
+            )
+
 
 if __name__ == "__main__":
     import doctest
