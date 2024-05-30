d9330ef0b3c348a941d42e8974dc9807dd154114
watch14”git config --global user.name watch14” <watchman1414@gmail.com>
2024-05-30 03:36:22 +0100

    fast_push

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,8 +1,18 @@
 #!/usr/bin/env python3
-""" filtered_logger """
-import logging
+"""
+filtered_logger module
+"""
+import re
+from typing import List
 
 
-def filter_datum():
-    """ filtred """
-    return
+def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
+    """
+    filtered dat
+    """
+    pattern = f"({'|'.join(fields)})=.+?{separator}"
+    return re.sub(pattern, lambda m: f"{m.group(1)}={redaction}{separator}", message)
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
