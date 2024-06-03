54dd1786fe1d2911a55b34982933b0e431db415c
watch14”git config --global user.name watch14” <watchman1414@gmail.com>
2024-06-03 10:16:31 +0100

    t7

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -8,4 +8,15 @@ from .auth import Auth
 
 class BasicAuth(Auth):
     """ inherit from Auth class """
-    pass
+    def extract_base64_authorization_header(self, authorization_header: str) -> str:
+        """ BasicAuth that returns the Base64 """
+        if authorization_header is None :
+            return None
+        
+        if not isinstance(authorization_header, str):
+            return None
+
+        if not authorization_header.startswith("Basic "):
+            return None
+
+        return authorization_header.split(" ")[1]
