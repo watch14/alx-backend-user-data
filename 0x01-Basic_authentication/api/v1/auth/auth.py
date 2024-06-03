035aad073918e166bb927ea29fb07842e4468af8
watch14”git config --global user.name watch14” <watchman1414@gmail.com>
2024-06-03 09:04:00 +0100

    auth r4

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -14,7 +14,17 @@ class Auth:
         """ require_auth
         returns False - path
         """
-        return False
+        if path is None:
+            return True
+
+        if excluded_paths is None or len(excluded_paths) == 0:
+            return True
+
+        for excluded_path in excluded_paths:
+            if path.rstrip('/') == excluded_path.rstrip('/'):
+                return False
+
+        return True
 
     def authorization_header(self, request=None) -> str:
         """ authorization_header
