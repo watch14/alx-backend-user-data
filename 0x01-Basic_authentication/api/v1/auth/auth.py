5bb2a0bc5e334c82cadfb78cd46eac4e1caf5f8c
watch14”git config --global user.name watch14” <watchman1414@gmail.com>
2024-06-03 08:35:55 +0100

    auth t3

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+"""
+Auth class
+"""
+
+from typing import List, TypeVar
+from flask import request
+
+
+
+class Auth:
+    """ Auth Class """
+    
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """ require_auth
+        returns False - path
+        """
+        return False
+    
+    
+    def authorization_header(self, request=None) -> str:
+        """ authorization_header
+         that returns None - request
+        """
+        return None
+    
+    
+    def current_user(self, request=None) -> TypeVar('User'):
+        """  current_user
+        returns None - request
+        """
+        return None
