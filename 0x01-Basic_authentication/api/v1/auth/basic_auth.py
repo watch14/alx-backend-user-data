2eb799507a8680871416c4bb6e01ab4e9376d317
watch14”git config --global user.name watch14” <watchman1414@gmail.com>
2024-06-03 10:36:30 +0100

    t9

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -5,6 +5,7 @@ Basic Auth class
 
 from .auth import Auth
 import base64
+from typing import Tuple
 
 
 class BasicAuth(Auth):
@@ -37,3 +38,20 @@ class BasicAuth(Auth):
             return decoded.decode('utf-8')
         except base64.binascii.Error:
             return None
+
+    def extract_user_credentials(
+                self, decoded_base64_authorization_header: str) -> (str, str):
+
+        """ returns the user email and password from the Base64 """
+        if decoded_base64_authorization_header is None:
+            return None, None
+
+        if not isinstance(decoded_base64_authorization_header, str):
+            return None, None
+
+        if ':' not in decoded_base64_authorization_header:
+            return None, None
+
+        email, name = decoded_base64_authorization_header.split(":")
+
+        return email, name
