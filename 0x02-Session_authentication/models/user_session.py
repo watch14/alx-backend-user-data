036f997f20d46ed6b98775d61aa75417db09594c
watch14”git config --global user.name watch14” <watchman1414@gmail.com>
2024-06-06 18:05:07 +0100

    10.?????????????

---
+++ 0x02-Session_authentication/models/user_session.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+Sessions in database
+"""
+from models.base import Base
+
+
+class UserSession(Base):
+    """ UserSession """
+
+    def __init__(self, *args: list, **kwargs: dict):
+        pass
