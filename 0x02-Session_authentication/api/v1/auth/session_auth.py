d268cb5c99abe8a9a2a6482a52690d5298645d9a
watch14”git config --global user.name watch14” <watchman1414@gmail.com>
2024-06-06 16:38:29 +0100

    t2

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -3,8 +3,17 @@
 session Auth class
 """
 from .auth import Auth
+import uuid
 
 
 class SessionAuth(Auth):
     """ session authorizer """
-    pass
+    user_id_by_session_id  = {}
+    def create_session(self, user_id: str = None) -> str:
+        """ create session """
+        if user_id is None or not isinstance(user_id, str):
+            return None
+
+        user_id = uuid.uuid4()
+        self.user_id_by_session_id[str(id)] = user_id
+        return str(id)
