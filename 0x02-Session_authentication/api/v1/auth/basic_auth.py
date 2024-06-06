ecb302b7b71716fc4ab3ad3bf8edf7cc32df9eb6
watch14”git config --global user.name watch14” <watchman1414@gmail.com>
2024-06-06 15:34:16 +0100

    fast_push

--- 0x02-Session_authentication/api/v1/auth/basic_auth.py
+++ 0x02-Session_authentication/api/v1/auth/basic_auth.py
@@ -73,9 +73,9 @@ class BasicAuth(Auth):
             return None
 
         for user in users:
-            if user.is_valid_password(user_pwd):
-                return user
-        return None
+            if  not user.is_valid_password(user_pwd):
+                return None
+        return user
 
     def current_user(self, request=None) -> TypeVar('User'):
         """ overloads Auth and retrieves the User """
