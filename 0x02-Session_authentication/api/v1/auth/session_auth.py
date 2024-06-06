d2139b2b9221aad088655e40c07c0cb274b7f715
watch14”git config --global user.name watch14” <watchman1414@gmail.com>
2024-06-06 16:53:44 +0100

    t3.1

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -21,7 +21,7 @@ class SessionAuth(Auth):
 
     def user_id_for_session_id(self, session_id: str = None) -> str:
         """ user_id_for_session_id """
-        if session_id  is None or not isinstance(session_id , str):
+        if session_id is None or not isinstance(session_id, str):
             return None
 
         return self.user_id_by_session_id.get(session_id)
