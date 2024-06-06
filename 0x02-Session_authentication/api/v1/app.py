b48389ff1a8f2fd62a394405217d84fad74e7517
watch14”git config --global user.name watch14” <watchman1414@gmail.com>
2024-06-06 17:21:57 +0100

    t4.0

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -46,11 +46,14 @@ def bef_req():
     setattr(request, "current_user", auth.current_user(request))
     exclu_paths = ['/api/v1/status/',
                    '/api/v1/unauthorized/',
-                   '/api/v1/forbidden/']
+                   '/api/v1/forbidden/',
+                   '/api/v1/auth_session/login/',
+                   ]
 
     if auth.require_auth(request.path, exclu_paths):
 
-        if auth.authorization_header(request) is None:
+        cookies = auth.session_cookie(request)
+        if auth.authorization_header(request) is None and cookies is None:
             abort(401)
 
         if auth.current_user(request) is None:
