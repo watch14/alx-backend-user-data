3b9c95942460f9f2c9359d51ce8251aaa926ec2c
watch14”git config --global user.name watch14” <watchman1414@gmail.com>
2024-06-07 12:46:38 +0100

    fx

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -56,7 +56,7 @@ def bef_req():
     exclu_paths = ['/api/v1/status/',
                    '/api/v1/unauthorized/',
                    '/api/v1/forbidden/',
-                   '/api/v1/auth_session/login/',
+                   '/api/v1/auth_session/login/'
                    ]
 
     if auth.require_auth(request.path, exclu_paths):
