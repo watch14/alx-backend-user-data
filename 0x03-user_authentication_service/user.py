76da7b743f028f01aac825d79e55ffc256cd9bf4
watch14”git config --global user.name watch14” <watchman1414@gmail.com>
2024-06-13 13:17:42 +0100

    t0 fixed

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -10,8 +10,8 @@ Base = declarative_base()
 class User(Base):
     """ User database model """
     __tablename__ = 'users'
-    
-    id = Column(Integer, primary_key = True)
+
+    id = Column(Integer, primary_key=True)
     email = Column(String(250), nullable=False)
     hashed_password = Column(String(250), nullable=False)
     session_id = Column(String(250), nullable=False)
