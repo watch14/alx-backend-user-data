8500237c7931a829242df2de10447bf5a469517b
watch14”git config --global user.name watch14” <watchman1414@gmail.com>
2024-06-13 13:13:40 +0100

    Users

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -1,2 +1,18 @@
 #!/usr/bin/env python3
 """ User model """
+
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
+
+Base = declarative_base()
+
+
+class User(Base):
+    """ User database model """
+    __tablenmame__ = 'users'
+    
+    id = Column(Integer, primary_key = True)
+    email = Column(String, nullable=False)
+    hashed_password = Column(String, nullable=False)
+    session_id = Column(String, nullable=False)
+    reset_token = Column(String, nullable=False)
